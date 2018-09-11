5eb512760cf7fae9a087dec464938abf3816c69f
ed.a.ezekiel@gmail.com <ed.a.ezekiel@gmail.com>
2018-09-11 12:14:14 +0000

    automatically backed up by learn

--- hoisting.js
+++ hoisting.js
@@ -16,6 +16,13 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
 
+  sayMy();
+
+  function sayMy() {
     var name = "Kristin";
+
     console.log(name);
+    var name = "Cricky";
+
+  }
 }
